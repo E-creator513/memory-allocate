5a71dea776855b58a4368dd0680439315b9f7314
Emmanuel Zvavamwe <76424176+E-creator513@users.noreply.github.com>
2022-01-30 16:08:18 +0300

    Update bmp_rotation.c

--- src/bmp_rotation.c
+++ src/bmp_rotation.c
@@ -6,11 +6,15 @@
 
 struct image rotate(struct image const img) {
     struct image img_rotate = {.width = img.height,.height = img.width};
-    img_rotate.data = malloc(img.height * img.width * sizeof(struct pixel));
+    img_rotate.data = malloc(img.height * img.width * sizeof(struct pixel));//Allocates size bytes of uninitialized storage.
+
+If allocation succeeds, returns a pointer that is suitably aligned for any object type with fundamental alignment.
     for (uint64_t row = 0; row < img.height; row++) {
+        
         for (uint64_t col = 0; col < img.width; col++) {
+            
             img_rotate.data[row+((img_rotate.height-col-1) * img_rotate.width)] = img.data[col+row * img.width];
-        }
+        }/*malloc is thread-safe: it behaves as though only accessing the memory locations visible through its argument, and not any static storage.*/
     }
     return img_rotate;
 }
